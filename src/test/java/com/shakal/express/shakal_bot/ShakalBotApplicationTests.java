583ab71e4bb1397211f63822aea09dd0eafe3906
yuliia.kyianytsia <u.kiyanitsa@gmail.com>
2025-03-19 14:46:20 +0200

    Initial commit

---
+++ src/test/java/com/shakal/express/shakal_bot/ShakalBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakal.express.shakal_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShakalBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
